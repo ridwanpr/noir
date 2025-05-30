8d53a6d1d2b6b4975b3c376c6d787d1e8ec50d04
Ridwan <ridwanpr@outlook.com>
2025-05-31 03:24:11 +0700

    fix svg

--- src/components/common/Icons.jsx
+++ src/components/common/Icons.jsx
@@ -53,9 +53,9 @@ export const UserIcon = () => {
   return (
     <svg fill="currentColor" viewBox="0 0 20 20">
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       ></path>
     </svg>
   );
